c36bbb1f67684c66f7456f781bd3bf34b9badeee
stephen <lonelytreesl@yahoo.com>
2015-06-12 21:07:10 +0000

    add spec and code for Artwork.museum_id() to make it pass, all specs passing.

--- lib/artwork.rb
+++ lib/artwork.rb
@@ -1,9 +1,10 @@
 class Artwork
   
-  attr_reader(:name, :description)
+  attr_reader(:name, :description, :museum_id)
   
   define_method(:initialize) do |attributes|
-    @name =attributes.fetch(:name)
-    @description =attributes.fetch(:description)
+    @name = attributes.fetch(:name)
+    @description = attributes.fetch(:description)
+    @museum_id = attributes.fetch(:museum_id)
   end
 end
--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -3,15 +3,22 @@ require('spec_helper')
 describe(Artwork) do 
   describe('#name') do 
     it('returns the name of an instance of Artwork') do 
-      test_artwork = Artwork.new(:name => "Two Trees", :description => "Clay mold of two trees.")
+      test_artwork = Artwork.new(:name => "Two Trees", :description => "Clay mold of two trees.", :museum_id => 1)
       expect(test_artwork.name()).to(eq("Two Trees"))
     end
   end
   
   describe('#description') do 
     it('returns the description of an instance of Artwork ') do 
-      test_artwork = Artwork.new(:name => "Halo", :description => "Black and white painting.")
+      test_artwork = Artwork.new(:name => "Halo", :description => "Black and white painting.", :museum_id =>1)
       expect(test_artwork.description()).to(eq("Black and white painting."))
+    end
+  end
+  
+  describe("#museum_id") do 
+    it('returns the id of the museum an instance of Artwork is associated with') do 
+      test_artwork = Artwork.new(:name => "Moonlight", :description => "Marble sculpture.", :museum_id => 1)
+      expect(test_artwork.museum_id()).to(eq(1))
     end
   end
 end
