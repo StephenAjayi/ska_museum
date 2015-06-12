c4ffdfbe7cc33ad4b34487cc476b211b621768b5
stephen <lonelytreesl@yahoo.com>
2015-06-12 23:11:40 +0000

    add spec and code for Artwork#id() to make it pass, all specs passing.

--- lib/artwork.rb
+++ lib/artwork.rb
@@ -1,11 +1,12 @@
 class Artwork
   
-  attr_reader(:name, :description, :museum_id)
+  attr_reader(:name, :description, :id, :museum_id)
   
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @description = attributes.fetch(:description)
     @museum_id = attributes.fetch(:museum_id)
+    @id = attributes.fetch(:id)
   end
   
   define_singleton_method(:all) do 
@@ -14,17 +15,19 @@ class Artwork
     all_artwork.each() do |artwork|
       name = artwork.fetch("name")
       description = artwork.fetch("description")
+      id = artwork.fetch("id").to_i()
       museum_id = artwork.fetch("museum_id").to_i()
-      artworks.push(Artwork.new(:name=> name, :description => description, :museum_id => museum_id))
+      artworks.push(Artwork.new(:name=> name, :description => description, :museum_id => museum_id, :id => id))
     end
     artworks
   end
   
   define_method(:save) do
-    saved_artwork = DB.exec("INSERT INTO artworks (name, description, museum_id) VALUES ('#{@name}', '#{@description}', '#{@museum_id}');")
+    saved_artwork = DB.exec("INSERT INTO artworks (name, description, museum_id) VALUES ('#{@name}', '#{@description}', '#{@museum_id}') RETURNING id;")
+    @id = saved_artwork.first().fetch("id").to_i() 
   end
   
   define_method(:==) do |another_artwork|
-    self.name().==(another_artwork.name).&(self.description().==(another_artwork.description)).&(self.museum_id().==(another_artwork.museum_id))
+    self.name().==(another_artwork.name).&(self.description().==(another_artwork.description)).&(self.museum_id().==(another_artwork.museum_id)).&(self.id().==(another_artwork.id()))
   end
 end
--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -3,21 +3,21 @@ require('spec_helper')
 describe(Artwork) do 
   describe('#name') do 
     it('returns the name of an instance of Artwork') do 
-      test_artwork = Artwork.new(:name => "Two Trees", :description => "Clay mold of two trees.", :museum_id => 1)
+      test_artwork = Artwork.new(:name => "Two Trees", :description => "Clay mold of two trees.", :museum_id => 1, :id => nil)
       expect(test_artwork.name()).to(eq("Two Trees"))
     end
   end
   
   describe('#description') do 
     it('returns the description of an instance of Artwork ') do 
-      test_artwork = Artwork.new(:name => "Halo", :description => "Black and white painting.", :museum_id =>1)
+      test_artwork = Artwork.new(:name => "Halo", :description => "Black and white painting.", :museum_id => 1, :id => nil)
       expect(test_artwork.description()).to(eq("Black and white painting."))
     end
   end
-  
+    
   describe("#museum_id") do 
     it('returns the id of the museum an instance of Artwork is associated with') do 
-      test_artwork = Artwork.new(:name => "Moonlight", :description => "Marble sculpture.", :museum_id => 1)
+      test_artwork = Artwork.new(:name => "Moonlight", :description => "Marble sculpture.", :museum_id => 1, :id => nil)
       expect(test_artwork.museum_id()).to(eq(1))
     end
   end
@@ -30,7 +30,7 @@ describe(Artwork) do
   
   describe('#save') do 
     it('saves an instance of Artwork into an array') do 
-      test_artwork = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1)
+      test_artwork = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1, :id => nil)
       test_artwork.save()
       expect(Artwork.all()).to(eq([test_artwork]))
     end
@@ -38,9 +38,17 @@ describe(Artwork) do
   
   describe('#==') do 
     it('evaluates two instances of Artwork as equal if they have the same name, description and museum_id') do
-      test_artwork = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1)
-      test_artwork2 = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1)
+      test_artwork = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1, :id => nil)
+      test_artwork2 = Artwork.new(:name => "painted Corners", :description => "Modern installation.", :museum_id => 1, :id => nil)
       expect(test_artwork).to(eq(test_artwork2))
+    end
+  end
+   
+  describe('#id') do 
+    it('returns the id of an instance of Artwork ') do 
+      test_artwork = Artwork.new(:name => "Halo", :description => "Black and white painting.", :museum_id => 1, :id => nil)
+      test_artwork.save()  
+      expect(test_artwork.id()).to(be_an_instance_of(Fixnum))
     end
   end
 end
